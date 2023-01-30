6850d88e78a483f1ff0df9d22036320c08b9b853
GerganaNoneva <gergananoneva@gmail.com>
2023-01-30 11:29:00 +0200

    Solution

--- src/App.js
+++ src/App.js
@@ -1,21 +1,28 @@
-import "./App.css";
+import React, { useState, useMemo } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-     <div className="control has-icons-right">
-        <input
-          className="input is-large"
-          type="text"
-          placeholder="Enter number..."
+  const [text, setText] = useState('');
 
-        />
-        <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
-      </div>
+  // Use memoization to determine if the input is a valid number
+  const isValidNumber = useMemo(() => {
+    const regEx = /^\d+$/;
+    return regEx.test(text);
+  }, [text]);
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={e => setText(e.target.value)}
+      />
+      <i
+        className={`fa ${isValidNumber ? 'fa-check' : 'fa-times'}`}
+        style={{ marginLeft: '10px' }}
+      />
     </div>
   );
 }
 
 export default App;
+
